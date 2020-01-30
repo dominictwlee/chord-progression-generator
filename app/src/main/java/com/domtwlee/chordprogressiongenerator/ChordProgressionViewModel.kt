5adb1aa108dd26c30ee23620af9265ce87d3d4b5
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2020-01-30 22:09:03 +0000

    create ChordProgressionViewModel

---
+++ app/src/main/java/com/domtwlee/chordprogressiongenerator/ChordProgressionViewModel.kt
@@ -0,0 +1,25 @@
+package com.domtwlee.chordprogressiongenerator
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.domtwlee.chordprogressiongenerator.database.ChordProgression
+import com.domtwlee.chordprogressiongenerator.database.ChordProgressionDatabase
+import com.domtwlee.chordprogressiongenerator.database.ChordProgressionRepository
+import kotlinx.coroutines.launch
+
+class ChordProgressionViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: ChordProgressionRepository
+    val chordProgressionList: LiveData<List<ChordProgression>>
+
+    init {
+        val chordProgressionDao = ChordProgressionDatabase.getDatabase(application).chordProgressionDao()
+        repository = ChordProgressionRepository(chordProgressionDao)
+        chordProgressionList = repository.chordProgressionList
+    }
+
+    fun insert(chordProgression: ChordProgression) = viewModelScope.launch {
+        repository.insert(chordProgression)
+    }
+}
--- app/src/main/java/com/domtwlee/chordprogressiongenerator/database/ChordProgressionRepository.kt
+++ app/src/main/java/com/domtwlee/chordprogressiongenerator/database/ChordProgressionRepository.kt
@@ -3,7 +3,7 @@ package com.domtwlee.chordprogressiongenerator.database
 import androidx.lifecycle.LiveData
 
 class ChordProgressionRepository(private val chordProgressionDao: ChordProgressionDao) {
-    val chordProgressionsList: LiveData<List<ChordProgression>> = chordProgressionDao.getChordProgressions()
+    val chordProgressionList: LiveData<List<ChordProgression>> = chordProgressionDao.getChordProgressions()
 
     suspend fun insert(chordProgression: ChordProgression) {
         chordProgressionDao.insert(chordProgression)
